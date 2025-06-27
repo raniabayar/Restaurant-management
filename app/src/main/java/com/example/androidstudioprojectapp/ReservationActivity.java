ea563755d2379635705221b3f76dfaf4089b44ec
raniabayar <bayarrania4@gmail.com>
2025-06-27 20:49:46 +0100

    Add ReservationActivity and related layouts

--- app/src/main/java/com/example/androidstudioprojectapp/ReservationActivity.java
+++ app/src/main/java/com/example/androidstudioprojectapp/ReservationActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidstudioproject;
+package com.example.androidstudioprojectapp;
 
 import android.os.Bundle;
 
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class ReservationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_reservation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
